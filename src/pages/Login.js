7b1a04a1b91f953864c458dd0f5357d4c08ec801
dzubi77 <mat.dzubak@gmail.com>
2024-10-20 16:12:37 +0200

    hopefully login page done :D

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,17 @@ function Login() {
     return (
         <>
             <Layout showTopContainer={false}>
-                <h1>Login page</h1>
+                <form className="login-form">
+                    <div className="mb-3 mt-3 username-container">
+                        <label for="username">Username:</label>
+                        <input type="text" className="form-control" id="username" placeholder="email@example.com"></input>
+                    </div>
+                    <div className="mb-3 password-container">
+                        <label for="password">Password:</label>
+                        <input type="password" className="form-control" id="password"></input>
+                    </div>
+                    <button className="btn btn-primary">Log in</button>
+                </form>
             </Layout>
         </>
     );
