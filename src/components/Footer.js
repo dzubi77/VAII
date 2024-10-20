417af7602183767922d81f0f24e77e5fcf1c1bf2
dzubi77 <mat.dzubak@gmail.com>
2024-10-20 22:15:52 +0200

    ready for check 1.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
                     In case of problems do not hesitate to contact me        
                 </p>
                 Matej Dzubak
-                <br></br>
-                <a href="/">mat.dzubak@gmail.com</a>
-                <br></br>
-                +421 949 275 272
+                <br />
+                <a href="/">example@gmail.com</a>
+                <br />
+                +421 000 000 000
             </div>
         </>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,17 +5,22 @@ function Login() {
     return (
         <>
             <Layout showTopContainer={false}>
-                <form className="login-form">
-                    <div className="mb-3 mt-3 username-container">
-                        <label for="username">Username:</label>
-                        <input type="text" className="form-control" id="username" placeholder="email@example.com"></input>
-                    </div>
-                    <div className="mb-3 password-container">
-                        <label for="password">Password:</label>
-                        <input type="password" className="form-control" id="password"></input>
-                    </div>
+                <div className="login-content">
+                    <form>
+                        <div className="mb-3 mt-3">
+                            <label for="username">Username:</label>
+                            <input type="text" className="form-control" id="username" placeholder="email@example.com"></input>
+                        </div>
+                        <div className="mb-3">
+                            <label for="password">Password:</label>
+                            <input type="password" className="form-control" id="password"></input>
+                        </div>
+                    </form>
                     <button className="btn btn-primary">Log in</button>
-                </form>
+                    <a href="/">Forgot password?</a>
+                    <p>Do not have an account?</p>
+                    <a href="/" type="button" className="btn btn-primary">Sign up</a>
+                </div>
             </Layout>
         </>
     );
