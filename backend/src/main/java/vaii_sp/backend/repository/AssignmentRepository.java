6a71005e88a09727f7c663728eb07885f1e0dcac
dzubi77 <mat.dzubak@gmail.com>
2025-02-04 12:06:23 +0100

    fixed rendering of courses based on role

--- backend/src/main/java/vaii_sp/backend/repository/AssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package vaii_sp.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vaii_sp.backend.model.Assignment;
-
-import java.util.UUID;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, UUID> {
-}
--- backend/src/main/java/vaii_sp/backend/repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package vaii_sp.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vaii_sp.backend.model.Feedback;
-
-import java.util.UUID;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, UUID> {
-}
